8a85cd9320b57c265f4bdd8666e778aee03ab863
Arpit kumar <rk9003278@gmail.com>
2022-11-27 13:26:31 +0530

    Add modified file

--- src/App.js
+++ src/App.js
@@ -1,25 +1,43 @@
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from 'react'
+import Headerkeep from './component/Headerkeep.js'
+import Notes from './component/Notes.js'
+import Notegroup from './component/Notegroup.js'
 
-function App() {
-  return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
-  );
+const App= () => {
+    const [alldata, setalldata] = useState([]);
+    const addnote = (note) => {
+        if ((note.name !== "") && (note.texts !== ""))
+            setalldata([...alldata, note]);
+
+        console.log(alldata);
+    }
+    const deletenote = (indexs) => {
+        const update = alldata.filter((element, index) => {
+            return indexs !== index;
+        })
+        setalldata(update);
+    }
+    return (
+        <div >
+            <Headerkeep name="Arpit Keep" />
+            <Notes passnote={addnote} />
+            <div className="notesbox">
+                {
+                    alldata.map((element, index) => {
+                        return (
+                            <Notegroup
+                                key={index}
+                                id={index}
+                                title={element.name}
+                                content={element.texts}
+                                delete={deletenote}
+                            />
+                        )
+                    })
+                }
+            </div>
+        </div>
+    )
 }
 
-export default App;
+export default App
