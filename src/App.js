19c26e0ee2bded6d9b995e1b9d4d717d6352c3d1
Arpit kumar <rk9003278@gmail.com>
2022-12-11 22:43:09 +0530

    Resolving Clear All Issues

--- src/App.js
+++ src/App.js
@@ -1,9 +1,8 @@
-import React, { useEffect, useState } from 'react'
+import React, { useState, useEffect } from 'react'
 import Headerkeep from './component/Headerkeep.js'
 import Notes from './component/Notes.js'
 import Notegroup from './component/Notegroup.js'
 import Footer from './component/Footer.js'
-
 
 const getlocalstorage = () => {
     let lists = localStorage.getItem('list');
@@ -23,14 +22,25 @@ const getlocalcheck = () => {
         return 0;
     }
 }
+const getlocalflag = () => {
+    let showdatas = localStorage.getItem('showdata');
+    if (showdatas) {
+        return JSON.parse(localStorage.getItem('showdata'));
+    }
+    else {
+        return 0;
+    }
+}
 const App = () => {
     const [alldata, setalldata] = useState(getlocalstorage());
     let [data, setdata] = useState(getlocalcheck());
+    const [show, setshow] = useState(getlocalflag());
     const addnote = (note) => {
         if ((note.name !== "") && (note.texts !== "")) {
             setalldata([...alldata, note]);
             data++
             setdata(data);
+            setshow(0);
         }
     }
     const deletenote = (indexs) => {
@@ -43,6 +53,8 @@ const App = () => {
     }
     const deleteall = () => {
         setalldata([]);
+        setshow(9);
+        setdata(0);
     }
     useEffect(() => {
         localStorage.setItem('list', JSON.stringify(alldata));
@@ -50,12 +62,15 @@ const App = () => {
     useEffect(() => {
         localStorage.setItem('item', JSON.stringify(data));
     }, [data])
+    useEffect(() => {
+        localStorage.setItem('showdata', JSON.stringify(show));
+    }, [show])
     return (
         <div >
             <Headerkeep name="Arpit Keep" />
             <Notes passnote={addnote} cleardata={deleteall} />
             <div className="notesbox">
-                {data === 0 ?
+                {data == 0 || show !== 0 ?
                     <div className="notesmsg"><p>When You Create Notes It Will Shown Here</p></div> :
                     alldata.map((element, index) => {
                         return (
